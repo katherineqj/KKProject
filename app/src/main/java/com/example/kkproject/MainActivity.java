5ec470e32897ef8131cee20b2b6d76366b3b27d1
qinjie03 <qinjie03@58.com>
2020-05-18 21:09:56 +0800

    建立项目

---
+++ app/src/main/java/com/example/kkproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kkproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
